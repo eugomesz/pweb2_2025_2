4db55fb8fccb611f23bf9f30b0948409ed57e424
eugomesz <alisson.lgg@aluno.ifsc.edu.br>
2025-09-12 13:33:04 -0300

    ajuste código professor

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -46,27 +46,42 @@ class AlunoController extends Controller
 
     public function edit(string $id)
     {
-        //
+        $dado = Aluno::findOrFail($id);
+        // dd($dado);
+
+        return view('aluno.form', ['dado' => $dado]);
     }
 
 
     public function update(Request $request, string $id)
     {
-        //
+        //dd($request->all());
+        $request->validate([
+            'nome' => 'required',
+            'cpf' => 'required',
+        ], [
+            'nome.required' => 'O :attribute é obrigatório',
+            'cpf.required' => 'O :attribute é obrigatório',
+        ]);
+
+        Aluno::updateOrCreate(['id' => $id], $request->all());
+
+        return redirect('aluno');
     }
 
 
     public function destroy(string $id)
     {
-        $dado = Aluno::find($id);
+        $dado = Aluno::findOrFail($id);
 
         $dado->delete();
 
         return redirect('aluno');
     }
 
-    public function search(Request $request){
-        if(!empty($request->valor)){
+    public function search(Request $request)
+    {
+        if (!empty($request->valor)) {
             $dados = Aluno::where(
                 $request->tipo,
                 'like',
--- resources/views/aluno/list.blade.php
+++ resources/views/aluno/list.blade.php
@@ -1,17 +1,16 @@
 @extends('base')
-@section('titulo', 'Listagem Aluno')
+@section('titulo', 'Listagem de Alunos')
 @section('conteudo')
 
-    <h3>Listagem de Alunoa</h3>
-
+    <h3>Listagem de Alunos</h3>
 
     <div class="row">
         <div class="col">
-            <form action="{{route('aluno.store')}}" method="post">
+            <form action="{{ route('aluno.search') }}" method="post">
                 @csrf
                 <div class="row">
                     <div class="col-md-3">
-                        <label for="">Tipo</label>
+                        <label class="form-label">Tipo</label>
                         <select name="tipo" class="form-select">
                             <option value="nome">Nome</option>
                             <option value="cpf">CPF</option>
@@ -19,67 +18,64 @@
                         </select>
 
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-4">
                         <label class="form-label">Valor</label>
                         <input type="text" class="form-control" name="valor" placeholder="Pesquisar...">
                     </div>
-                </div>
-                <div class="col-md-4">
-                    <label class="form-label">Valor</label>
-                    <input type="text" class="form-control" name"valor" placeholder=""
-                </div>
 
-                <div class="col-md-3">
-                    <button type="submit" class="btn btn-primary">
-                        Buscar
-                    </button>
+                    <div class="col-md-3">
+                        <button type="submit" class="btn btn-primary">
+                            <i class="fa-solid fa-magnifying-glass"></i> Buscar
+                        </button>
+                    </div>
+                    <div class="col-md-3">
+                        <a class="btn btn-success" href="{{ url('/aluno/create') }}"> <i class="fa-solid fa-plus"></i>
+                            Novo</a>
+                    </div>
                 </div>
-                <div>
-                    class
-                </div>
+            </form>
 
-                </form>
-        </div>
-        <div class="col">
-            <a class="btn btn-success" href="{{url("/aluno/create")}}">Novo</a>
         </div>
     </div>
 
     <div class="row">
 
-    <table class="table table-hover">
-        <thead>
-            <tr>
-                <td>#ID</td>
-                <td>Nome</td>
-                <td>CPF</td>
-                <td>telefone</td>
-                <td>Ação</td>
-                <td>Ação</td>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($dados as $item)
+        <table class="table table-hover">
+            <thead>
                 <tr>
-                    <td>{{$item->id}}</td>
-                    <td>{{$item->nome}}</td>
-                    <td>{{$item->cpf}}</td>
-                    <td>{{$item->telefone}}</td>
-                    <td>Editar</td>
-                    <td>
-                        <form action="{{('aluno.destroy',$item->id)}}" method="post">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-sm btn-outline-danger"
-                                onclick="return confirm('Deseja Remover o registro?')"
-                            ><i class="fas fa-trash"></i></button>
-                        </form>
-                    </td>
+                    <td>#ID</td>
+                    <td>Nome</td>
+                    <td>CPF</td>
+                    <td>telefone</td>
+                    <td>Ação</td>
+                    <td>Ação</td>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-</body>
-</html>
+            </thead>
+            <tbody>
+                @foreach ($dados as $item)
+                    <tr>
+                        <td>{{ $item->id }}</td>
+                        <td>{{ $item->nome }}</td>
+                        <td>{{ $item->cpf }}</td>
+                        <td>{{ $item->telefone }}</td>
+                        <td>
+                            <a href="{{ route('aluno.edit', $item->id) }}" class="btn btn-outline-warning">
+                                <i class="fa-solid fa-pen-to-square"></i>
+                            </a>
+                        </td>
+                        <td>
+                            <form action="{{ route('aluno.destroy', $item->id) }}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-sm btn-outline-danger"
+                                    onclick="return confirm('Deseja Remover o registro?')"> <i
+                                        class="fas fa-trash"></i></button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 
-
+@stop
